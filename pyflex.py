7d5b29eb2f2843512296a1614fb0a7f78e7bdcaf
juliavilmor <93728952+juliavilmor@users.noreply.github.com>
2022-03-09 17:07:06 +0100

    jaj

---
+++ pyflex.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(description="This program does blablablabla")
+
+requiredNamed = parser.add_argument_group("Required arguments")
+
+requiredNamed.add_argument('-i', '--input',
+                    dest = "infile",
+                    action = "store",
+                    default = None,
+                    required = True,
+                    help = "Input FASTA file/Uniprot Id")
+
+requiredNamed.add_argument('-f', '--format',
+                            required = True,
+                            help = "Input format")
+
+parser.add_argument('-o', '--output',
+                    dest = "outfile",
+                    action = "store",
+                    default = "output.pdb",
+                    help = "Output file")
+
+parser.add_argument('-g', '--graph',
+                    dest = "outgraph",
+                    action = "store",
+                    default = None,
+                    help = "Graphical result")            
+
+options = parser.parse_args()
+
+
+print(options.infile)
